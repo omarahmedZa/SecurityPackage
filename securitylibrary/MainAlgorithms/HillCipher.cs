adcdc7bf14573ca43cafbeec430f1a9108985756
omar <omarzaafan732@gmail.com>
2023-03-08 18:38:05 +0200

    hillCipher Enc done

--- securitylibrary/MainAlgorithms/HillCipher.cs
+++ securitylibrary/MainAlgorithms/HillCipher.cs
@@ -25,13 +25,54 @@ namespace SecurityLibrary
 
         public List<int> Encrypt(List<int> plainText, List<int> key)
         {
-            throw new NotImplementedException();
+            List<int> cipherText = new List<int>();
+
+            //dot product
+
+            double keyDimentions = matrixDimentions(key.Count);
+
+            int halfResult, result = 0, counter = 0;
+
+            for (int i = 0; i < plainText.Count; i++)
+            {
+                counter++;
+                halfResult = plainText[i] * key[counter - 1];
+                result += halfResult;
+
+                if ((counter) % keyDimentions == 0)
+                {
+                    cipherText.Add(result % 26);
+                    //Console.WriteLine(result);
+                    result = 0;
+
+
+                    if ((counter) % key.Count != 0)
+                    {
+                        i -= int.Parse(keyDimentions.ToString());
+                    }
+                    
+                }
+
+                if ((counter) % key.Count == 0)
+                {
+                    counter = 0;
+                }
+
+                
+            }
+
+            return cipherText;
         }
 
 
         public List<int> Analyse3By3Key(List<int> plainText, List<int> cipherText)
         {
             throw new NotImplementedException();
+        }
+
+        public double matrixDimentions(double listCounter)
+        {
+            return Math.Sqrt(listCounter);
         }
 
     }
