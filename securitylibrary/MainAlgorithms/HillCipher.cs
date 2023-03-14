0af0ad53325b35a95d9bc1e7de968aec3093ce99
omar <omarzaafan732@gmail.com>
2023-03-14 03:23:09 +0200

    hill cipher Dec Done

--- securitylibrary/MainAlgorithms/HillCipher.cs
+++ securitylibrary/MainAlgorithms/HillCipher.cs
@@ -20,7 +20,34 @@ namespace SecurityLibrary
         public List<int> Decrypt(List<int> cipherText, List<int> key)
         {
             List<int> plainText = new List<int>();
+            List<int> keyInv = matrixInverse(key);
 
+            double keyDimentions = matrixDimentions(key.Count);
+
+            int halfResult, result = 0, counter = 0;
+
+            for (int i = 0; i < cipherText.Count; i++)
+            {
+                counter++;
+                halfResult = cipherText[i] * keyInv[counter - 1];
+                result += halfResult;
+
+                if ((counter) % keyDimentions == 0)
+                {
+                    plainText.Add(result % 26);
+                    result = 0;
+
+                    if ((counter) % keyInv.Count != 0)
+                    {
+                        i -= int.Parse(keyDimentions.ToString());
+                    }
+                }
+
+                if ((counter) % keyInv.Count == 0)
+                {
+                    counter = 0;
+                }
+            }
 
             return plainText;
         }
@@ -81,30 +108,48 @@ namespace SecurityLibrary
 
             if (keyDimentions < 3)
             {
-                determinand = (key[0] * key[2] - key[1] * key[3]) % 26;
+                determinand = (key[0] * key[2] - key[1] * key[3]);
             }
             else
             {
                 determinand = (key[0] * (key[4] * key[8] - key[5] * key[7])
                     - key[1] * (key[3] * key[8] - key[5] * key[6])
-                    + key[2] * (key[3] * key[7] - key[4] * key[6])) % 26;
+                    + key[2] * (key[3] * key[7] - key[4] * key[6]));
+            }
+
+            if (determinand < 0)
+            {
+                for (; determinand < 0; determinand += 26) ;
+            }
+            else
+            {
+                determinand = determinand % 26;
             }
 
             return determinand;
         }
 
-        public double multiplicativeInverse(double determinand)
+        public double multiplicativeInverse(double determinand, int mDim)
         {
 
             double b, c;
 
             int i;
 
-            for (i = 1; (i / (26 - determinand)) == (int)(i / (26 - determinand)); i += 26) ;
+            if (mDim > 2)
+            {
+                for (i = 1; (i / (26 - determinand)) != (int)(i / (26 - determinand)); i += 26) ;
+                
+                c = i / (26 - determinand);
+                b = 26 - c;
+            }
+            else
+            {
+                for (i = 1; (i / (26 - determinand)) != (int)(i / (26 - determinand)); i ++) ;
 
-            c = i / (26 - determinand);
-            b = 26 - c;
-
+                c = i / (26 - determinand);
+                b = 26 - c;
+            }
             return b;
         }
 
@@ -124,19 +169,28 @@ namespace SecurityLibrary
         public int elementDeterminand(int row, int col, int[,] matrix, int mDim)
         {
             int determinand;
-            int[,] dMatrix = new int[mDim - 1, mDim - 1];
+            List<int> dMatrix = new List<int>();
             for (int i = 0; i < mDim; i++)
             {
                 for (int j = 0; j < mDim; j++)
                 {
                     if(i != row && j != col)
                     {
-                        dMatrix[i, j] = matrix[i, j];
+                        dMatrix.Add(matrix[i, j]);
                     }
                 }
             }
 
-            determinand = (dMatrix[0, 0] * dMatrix[1, 1] - dMatrix[0, 1] * dMatrix[1, 0]);
+            determinand = (dMatrix[0] * dMatrix[3] - dMatrix[1] * dMatrix[2]);
+
+            if (determinand < 0)
+            {
+                for (; determinand < 0; determinand += 26) ;
+            }
+            else
+            {
+                determinand = determinand % 26;
+            }
 
             return determinand;
         }
@@ -148,37 +202,78 @@ namespace SecurityLibrary
             int[,] matrix = new int [mDim, mDim];
             int[,] matrixInv = new int[mDim, mDim];
             int[,] matrixTranc = new int[mDim, mDim];
+            
 
             double determinand = matrixDeterminand(key);
-            double b = multiplicativeInverse(determinand);
             transformToMatrix(key, matrix);
+            double b = multiplicativeInverse(determinand, mDim);
 
-
-            for (int i = 0; i < mDim; i++)
+            if (mDim > 2)
             {
-                for (int j = 0; j < mDim; j++)
+                for (int i = 0; i < mDim; i++)
                 {
-                    matrixTranc[i, j] = ((int)b * (int)Math.Pow(-1, i + j) 
-                        * elementDeterminand(i, j, matrix, mDim)) % 26;
+                    for (int j = 0; j < mDim; j++)
+                    {
+                        matrixTranc[i, j] = ((int)b * (int)Math.Pow(-1, i + j)
+                            * elementDeterminand(i, j, matrix, mDim));
+
+                        if (matrixTranc[i, j] < 0)
+                        {
+                            for (; matrixTranc[i, j] < 0; matrixTranc[i, j] += 26) ;
+                        }
+                        else
+                        {
+                            matrixTranc[i, j] = matrixTranc[i, j] % 26;
+                        }
+                    }
+                }
+
+                for (int a = 0; a < mDim; a++)
+                {
+                    for (int c = 0; c < mDim; c++)
+                    {
+                        matrixInv[a, c] = matrixTranc[c, a];
+                    }
+                }
+
+
+                for (int d = 0; d < mDim; d++)
+                {
+                    for (int e = 0; e < mDim; e++)
+                    {
+                        inverse.Add(matrixInv[d, e]);
+                    }
                 }
             }
-
-            for (int a = 0; a < mDim; a++)
+            else
             {
-                for (int c = 0; c < mDim; c++)
+                matrixInv[0, 0] = matrix[1, 1];
+                matrixInv[1, 1] = matrix[0, 0];
+                matrixInv[0, 1] = -matrix[0, 1];
+                matrixInv[1, 0] = -matrix[1, 0];
+
+                
+
+                for (int i = 0; i < mDim; i++)
                 {
-                    matrixInv[a, c] = matrixTranc[c, a];
+                    for (int j = 0; j < mDim; j++)
+                    {
+                        matrixTranc[i, j] = matrixInv[i, j] * (int)b;
+
+                        if (matrixTranc[i, j] < 0)
+                        {
+                            for (; matrixTranc[i, j] < 0; matrixTranc[i, j] += 26) ;
+                        }
+                        else
+                        {
+                            matrixTranc[i, j] = matrixTranc[i, j] % 26;
+                        }
+
+                        inverse.Add(matrixTranc[i, j]);
+                    }
                 }
             }
-
-
-            for (int d = 0; d < mDim; d++)
-            {
-                for (int e = 0; e < mDim; e++)
-                {
-                    inverse.Add(matrixInv[d, e]);
-                }
-            }
+            
 
             return inverse;
         }
